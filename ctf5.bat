a69d3d1f962b89619a27c78bee74d72d86db7f48
Jenish Maharjan <98453046+legendofzer0@users.noreply.github.com>
2025-02-14 18:41:53 +0545

    part2

---
+++ ctf5.bat
@@ -0,0 +1,26 @@
+@echo off
+setlocal enabledelayedexpansion
+
+set "Jsdklf93ksdFLAG=X"
+set "Lsdjfl93ksdCHUNK=TF"
+set "Kasdfk2930PART=-9"
+set "Aosdf932jfsdfMIX=00"
+set "KJadf9sd03VAR=-H"
+set "Jksdf0293HIDDEN=AC"
+set "Jadsf920fseCRET=KMC"
+set "Mjdf823jREVEAL={}"
+
+set "Xsdfl093errorOne=MissingVar"
+set "Ljdsf902errorTwo="
+set "Jsdfl923errorThree="
+
+echo Running diagnostics...
+ping 127.0.0.1 -n 2 >nul
+echo Checking system integrity...
+ping 127.0.0.1 -n 2 >nul
+echo Processing request...
+ping 127.0.0.1 -n 2 >nul
+
+set "HackedFlag=!Jsdklf93ksdFLAG!!Lsdjfl93ksdCHUNK!!Kasdfk2930PART!!Aosdf932jfsdfMIX!!KJadf9sd03VAR!!Jksdf0293HIDDEN!!Jadsf920fseCRET!"
+
+exit
